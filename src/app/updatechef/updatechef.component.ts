392e44a112ca9ae77707ad97a2bb7b4b2897b4a9
MuhammadZahid9596 <zahidnasim@live.com>
2019-08-06 05:53:09 +0500

    c

--- src/app/updatechef/updatechef.component.ts
+++ src/app/updatechef/updatechef.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { AppService } from '../app.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-updatechef',
@@ -7,9 +10,64 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UpdatechefComponent implements OnInit {
 
-  constructor() { }
+  abc: any;
+  updateFormChef: FormGroup;
 
-  ngOnInit() {
+  get customer_name() {
+      return this.updateFormChef.get('customer_name');
+  }
+  get customer_no() {
+    return this.updateFormChef.get('customer_no');
+  }
+  get customer_contact() {
+    return this.updateFormChef.get('customer_contact');
+  }
+  get email() {
+    return this.updateFormChef.get('email');
+  }
+  get password() {
+    return this.updateFormChef.get('password');
+  }
+  get confirmPassword() {
+    return this.updateFormChef.get('confirmPassword');
+  }
+  get customer_address() {
+    return this.updateFormChef.get('customer_address');
   }
 
+  constructor(private _appservice: AppService, private fb: FormBuilder, private router: Router) { }
+
+  ngOnInit() {
+
+    this.updateFormChef = this.fb.group({
+      customer_name: ['', [Validators.required, Validators.minLength(3)]],
+      customer_no: ['', [Validators.required, Validators.minLength(8)]],
+      customer_contact: ['', [Validators.required, Validators.minLength(11)]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+      confirmPassword: ['', [Validators.required]],
+      customer_address: ['', [Validators.required, Validators.minLength(15)]]
+
+    }, {
+      validator: this.MustMatch('password', 'confirmPassword')
+  });
+}
+MustMatch(controlName: string, matchingControlName: string) {
+  return (formGroup: FormGroup) => {
+      const control = formGroup.controls[controlName];
+      const matchingControl = formGroup.controls[matchingControlName];
+
+      if (matchingControl.errors && !matchingControl.errors.mustMatch) {
+          // return if another validator has already found an error on the matchingControl
+          return;
+      }
+
+      // set error on matchingControl if validation fails
+      if (control.value !== matchingControl.value) {
+          matchingControl.setErrors({ mustMatch: true });
+      } else {
+          matchingControl.setErrors(null);
+      }
+  };
+}
 }
--- src/app/updatecustomer/updatecustomer.component.ts
+++ src/app/updatecustomer/updatecustomer.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { AppService } from '../app.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-updatecustomer',
@@ -7,9 +10,64 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UpdatecustomerComponent implements OnInit {
 
-  constructor() { }
+  abc: any;
+  updateFormCustomer: FormGroup;
 
-  ngOnInit() {
+  get customer_name() {
+      return this.updateFormCustomer.get('customer_name');
+  }
+  get customer_no() {
+    return this.updateFormCustomer.get('customer_no');
+  }
+  get customer_contact() {
+    return this.updateFormCustomer.get('customer_contact');
+  }
+  get email() {
+    return this.updateFormCustomer.get('email');
+  }
+  get password() {
+    return this.updateFormCustomer.get('password');
+  }
+  get confirmPassword() {
+    return this.updateFormCustomer.get('confirmPassword');
+  }
+  get customer_address() {
+    return this.updateFormCustomer.get('customer_address');
   }
 
+  constructor(private _appservice: AppService, private fb: FormBuilder, private router: Router) { }
+
+  ngOnInit() {
+
+    this.updateFormCustomer = this.fb.group({
+      customer_name: ['', [Validators.required, Validators.minLength(3)]],
+      customer_no: ['', [Validators.required, Validators.minLength(8)]],
+      customer_contact: ['', [Validators.required, Validators.minLength(11)]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+      confirmPassword: ['', [Validators.required]],
+      customer_address: ['', [Validators.required, Validators.minLength(15)]]
+
+    }, {
+      validator: this.MustMatch('password', 'confirmPassword')
+  });
+}
+MustMatch(controlName: string, matchingControlName: string) {
+  return (formGroup: FormGroup) => {
+      const control = formGroup.controls[controlName];
+      const matchingControl = formGroup.controls[matchingControlName];
+
+      if (matchingControl.errors && !matchingControl.errors.mustMatch) {
+          // return if another validator has already found an error on the matchingControl
+          return;
+      }
+
+      // set error on matchingControl if validation fails
+      if (control.value !== matchingControl.value) {
+          matchingControl.setErrors({ mustMatch: true });
+      } else {
+          matchingControl.setErrors(null);
+      }
+  };
+}
 }
